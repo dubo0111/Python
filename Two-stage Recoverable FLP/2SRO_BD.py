610e74d1380b198db1369ab0f67dcdcba59e73b2
dubo0111 <dubo0111@hotmail.com>
2018-08-02 22:11:12 +0800

    upd

--- Two-stage Recoverable FLP/2SRO_BD.py
+++ Two-stage Recoverable FLP/2SRO_BD.py
@@ -12,7 +12,7 @@ import data_generator1 as dg
 #INPUT Parameters:p, cost matrix
 #p,cd = dg.ins_small()
 #p,cd = dg.ins_big(5)
-p,cd,cdk,sk = dg.ins_k(6,5) #(ni,nk,sumk)
+p,cd,cdk,sk = dg.ins_k(3,2) #(ni,nk,sumk)
 # !!!!! Make sure index match: cdk VS. v_ij(k) [k][i][j]
 from gurobipy import *
 #import os
@@ -83,69 +83,39 @@ try:
     #(1) Q(k) = sum_i(sum_j(y_j*gamma_kij))+sumsum_ij((1-a_kj)*delta_kij
     #           + sum_i(epsilon_ki) + sum_j((1-y_j)*lambda_kj) +
     #           + sum_j((1-a_kj)*mu_kj) + (p+sum_j(a_kj*y_j)-sum(y_j))*nu_k   forall k\
-    # c_gamma = []
-    # for i in range(ni):
-    #     c_gamma = c_gamma.extend(y)
-    c_delta = [1-sk[k][j] for k in range(nk) for j in range(ni)]
-    c_lamda = [1-value_y[j]] for j in range(ni)]
-    c_mu = c_delta.copy()
-    c_nu = p + sum([sk[k][j]*value_y[j] for j in range(ni)]) - sum(value_y)
+    ##
+    value_y = [1,0,0]
+    c_delta = [[0 for i in range(ni*ni)] for k in range(nk)]
+    for k in range(nk):
+        c_delta[k] = [1-sk[k][j] for j in range(ni)]*ni
+    c_lamda = [1-value_y[j] for j in range(ni)]
+    c_mu = [[0 for i in range(ni)] for k in range(nk)]
+    for k in range(nk):
+        c_mu[k] = [1-sk[k][j] for j in range(ni)]
+    c_nu=[]
+    for k in range(nk):
+        c_nu.append(p + sum([sk[k][j]*value_y[j] for j in range(ni)]) - sum(value_y))
     m1.addConstrs(
-            (Qk[k] == LinExpr(value_y*ni,gamma.select(k,'*','*')) + \
-            LinExpr(c_delta[k]*ni,delta.select(k,'*','*')) + \
-            epsilon.sum(k,*) + LinExpr(c_lamda,lamda.select(k,'*')) + \
-            LinExpr(c_mu,mu.select(k,'*')) + c_nu*nu(k) for k in range(nk)),
-            "Q(k)")
+             (Qk[k] == LinExpr(value_y*ni,gamma.select(k,'*','*')) + \
+             LinExpr(c_delta[k],delta.select(k,'*','*')) + \
+             epsilon.sum(k,'*') + LinExpr(c_lamda,lamda.select(k,'*')) + \
+             LinExpr(c_mu[k],mu.select(k,'*')) + c_nu[k]*nu[k] for k in range(nk)),
+             "Q(k)")
     #(2) -gamma_kij+lambda_kj+mu_kj+nu_k<=0  forall k,i,j
-
+    m1.addConstrs(
+            (-gamma[k,i,j]+lamda[k,j]+mu[k,j]+nu[k] <= 0 for k in range(nk) for i in range(ni) for j in range (ni)),
+            "u")
     #(3) c_kij*d_ki*beta_ki+gamma_kij+delta_kij+epsilon_ki<=0  forall k,i,j
-
+    m1.addConstrs(
+            (cdk[k][i][j]*beta[k,i] + gamma[k,i,j] + delta[k,i,j] +epsilon[k,i] <=0 for k in range(nk) for i in range(ni) for j in range(ni)),
+            "v")
     #(4) -sum_i(beta_i)<=1 forall k
     m1.addConstrs(
-
-    )
-    m1.addConstrs(
-            (eta >= L3[k] for k in range(nk)),
-            "eta>k")
-    #(6) L3(k) >= c'd'v(k) forall i,k
-    cdv = v.copy()
-    for k in range(nk):
-        for i in range(ni):
-            for j in range(ni):
-               cdv[k,i,j]=cdk[k][i][j]
-    m1.addConstrs(
-            (v.prod(cdv,k,i,'*') <= L3[k] for k in range(nk) for i in range(ni)),
-            "sumcdv<L3k")
-    #(7) v(k) <= y + u(k) forall k,i,j
-    m1.addConstrs(
-            (v[k,i,j] <= y[j] + u[k,j] for k in range(nk) for i in range(ni) for j in range(ni)),
-            "v<y+u")
-    #(8) v(k) <= 1 - a_j(k) forall k,i,j
-    m1.addConstrs(
-            (v[k,i,j] <= 1 - sk[k][j] for k in range(nk) for i in range(ni) for j in range(ni)),
-            "v<1-ak")
-    #(9) sum(v) = 1 forall i,k
-    m1.addConstrs(
-            (v.sum(k,i,'*') == 1 for k in range(nk) for i in range(ni)),
-            "sumv")
-    #(10) u(k) + y <= 1 forall k,j
-    m1.addConstrs(
-            (u[k,j] + y[j] <= 1 for k in range(nk) for j in range(ni)),
-            "u+y<1")
-    #(11) u(k) + a_j(k) <= 1 forall k,j
-    m1.addConstrs(
-            (u[k,j] + sk[k][j] <= 1 for k in range(nk) for j in range(ni)),
-            "u+ak<1")
-    #(12) sum(u(k)) + sum(y) - sum(a_j(k)*y) = p forall k (or <=)
-    m1.addConstrs(
-            (u.sum(k,'*') + y.sum() - LinExpr(sk[k],y.select()) == p for k in range(nk)),
-            "2S-p")
-
-
-
-
+            (-beta.sum(k,'*') <= 1 for k in range(nk)),
+            "L3")
+            
     # save model and optimize
-    m.write('sub.lp')
+    m1.write('sub.lp')
     #m.optimize()
 
     #Output
