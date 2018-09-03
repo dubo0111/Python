87f183a4756996de17f61e81d9af0f540638c36e
dubo0111 <39396419+dubo0111@users.noreply.github.com>
2018-09-03 21:46:39 +0800

    1

--- Two-stage Recoverable FLP/model_rflp.py
+++ Two-stage Recoverable FLP/model_rflp.py
@@ -592,7 +592,7 @@ class rflp:
         self.master_model.addConstr(
             self.a1 * self.master_model.getVars()[-1] + self.a1 * self.omega >= self.LB)
 
-    def initial_stablization(self, y_step=0.9, inter=0.1):
+    
         self.master(1)
         self.master_model.optimize()
         self.update_y()
